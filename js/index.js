99b91c04b2740e2f45046f95d1e2d2a6bb7ef919
Jessica <rayne1993@gmail.com>
2019-09-06 20:57:01 -0500

    Finished first 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const navStyle = document.querySelector("nav");
+navStyle.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "lightblue";
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 500);
+}, false);
+
+addEventListener("keydown", function( event ) {
+    this.document.body.style.background = "lightblue";
+    this.setTimeout(function() {
+        event.target.style.background = ""
+    }, 500);
+}, false);
+
+function zoom(event) {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    el.style.transform = `scale(${scale})`;
+  };
+  let scale = 1;
+  const el = document.querySelector(".intro img");
+  el.addEventListener('wheel', zoom);
